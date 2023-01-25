acb895e2ca6b6b8918a70fa6510ef4e1dd9d5584
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-01-25 05:57:41 +0000

    South and Southeast Asian solar New Year

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Feb 14, 2023 00:00:00").getTime();
+var countDownDate = new Date("Apr 14, 2023 00:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
