16009211d52526e98b932e0683ae2787db5bf982
Huto <92351427+Hutotpn@users.noreply.github.com>
2023-12-02 14:18:55 +0700

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 // * Set the date we're counting down to
-var countDownDate = new Date("Oct 23, 2023 10:25:00").getTime();
+var countDownDate = new Date("Dec 25, 2023 10:25:00").getTime();
 
 // Update the countdown every 1 second
 var loop = setInterval(countDown, 1000);
